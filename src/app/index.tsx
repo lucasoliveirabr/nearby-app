0922ff177d03c0ec933fb107f684011db2acaa1a
lucasoliveirabr <lucas.oliveirabr@outlook.com>
2024-12-15 21:55:36 +0000

    feat: add welcome screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,17 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import { Welcome } from "@/components/welcome";
+import { Steps } from "@/components/steps";
+import { Button } from "@/components/button";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Nearby App</Text>
+    <View style={{ flex: 1, padding: 40, gap: 40 }}>
+      <Welcome />
+      <Steps />
+
+      <Button>
+        <Button.Title>Começar</Button.Title>
+      </Button>
     </View>
   )
 }
---
+++ src/components/step/index.tsx
@@ -0,0 +1,22 @@
+import type { LucideProps } from "lucide-react-native";
+import { View, Text } from "react-native";
+import { colors } from "@/styles/theme";
+import { styles } from "./styles";
+
+type StepProps = {
+  title: string;
+  description: string;
+  icon: React.ComponentType<LucideProps>;
+}
+
+export function Step({ title, description, icon: Icon }: StepProps) {
+  return (
+    <View style={styles.container}>
+      {Icon && <Icon size={32} color={colors.red.base} />}
+      <View style={styles.details}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+    </View>
+  )
+}
